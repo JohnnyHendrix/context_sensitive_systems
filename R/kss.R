6cb15e771ff9660acd273b53144562d6fce7fc33
Jean-Marc Hendrikse <jeanmarc.hendrikse@googlemail.com>
2018-01-18 09:15:54 +0100

    Added Sliding Window with feature calculation

--- R/kss.R
+++ R/kss.R
@@ -26,85 +26,119 @@ dataset = influxdbr2::influx_select(con = con,
                                       db = "kss",
                                       value = "*",
                                       from = "kss_kay",
-                                      return_xts = FALSE)
-dataset = as.data.frame(dataset)
+                                      return_xts = TRUE)
+
+# Funktion zur Berechnung von Features mit Sliding Window
+calc_features<- function(data,columns,features,width,by){
+  x<-foreach(s=columns,.combine=cbind) %:% 
+    foreach(f=features,.combine=cbind) %do% 
+    {data.frame(rollapply(data=data[,s], width=width, by=by, FUN=get(f), na.rm=TRUE)) }
+  # Spaltennamen setzen
+  c<-foreach(s=columns,.combine=c) %:% 
+    foreach(f=features,.combine=c) %do% { paste(s,f,sep="_")}
+  colnames(x)[]<-c
+  return(x)  
+}
+
+dataset = data.frame(dataset)
 str(dataset)
 # Peek at the data for good measure
 head(dataset)
 
-#Sort columns -> user and context gets to the end and time, alpha, beta, gamma, x, y, z first
-dataset<-dataset[c(1,2,3,5,7,8,9,4,6)]
-
-# Rename column names
-names(dataset) <- c("time", "alpha", "beta", "gamma", "x", "y", "z", "context", "user")
-
-# catch all users in db
-users = unique(dataset$user)
-
 # catch all unqiue activities in our case 'gehen', 'stehen', 'treppe'
-context = unique(dataset$context)
 
 # cor(dataset[dataset$context==context[1],1:3])
 
 # get the dimension of the dataset to check
 dim(dataset)
 
-###########################  STEP 2 - CREATE VALIDATION SET ###########################
+
+###########################  STEP 2 - SLIDING WINDOW WITH MEAN AND SD ###########################
+
+activities = c("gehen", "stehen", "treppe")
+sensors = c("alpha", "beta", "gamma")
+users = unique(dataset$user_id)
+users = c("Kay")
+fun=c("mean","sd")
+w = 40
+users
+data=foreach(u=users,.combine=rbind)%:%
+  foreach(a=activities,.combine=rbind)%do%
+  {
+    d = filter(dataset, context == a & user_id == u)
+    t=calc_features(d,sensors,fun,w,w/2)
+    l=data.frame(d[seq(w-1,nrow(d),w/2),"context"],rep(u,length.out=nrow(t)))
+    colnames(l) = c("label","user")
+    cbind(l,t)
+    return(cbind(l,t))
+    
+  }
+
+library(pmml)
+library(rpart)
+head(data)
+my.rpart <- rpart(label ~ ., data=data)
+my.rpart
+pmml(my.rpart)
+saveXML(pmml(my.rpart), file = "kss.xml")
+
+###########################  STEP 3 - CREATE VALIDATION SET ###########################
 
 # Create a list of 80% of the rows in the original dataset we can use for training
-validation_index<-createDataPartition(dataset$context, p =0.80, list = FALSE)
+validation_index<-createDataPartition(dataset$context, p =.8, list = FALSE, times = 1)
 
 # Select 20% of the data for validation
-validation<-dataset[-validation_index, ]
-head(validation)
+test<-dataset[-validation_index, ]
+
 # Use the remaining 80% of data to train and test the models
-dataset<-dataset[validation_index, ]
+train<-dataset[validation_index, ]
 
-###########################  STEP 3 - SUMMARIZE DATASET ###########################
+###########################  STEP 4 - SUMMARIZE DATASET ###########################
 
-# Check again the dimension of the dataset and see if the dataset is reduced to 80%
-dim(dataset)
+# Check again the dimension of the train dataset and see if the train dataset is reduced to 80%
+dim(train)
 
 
 # TODO DIM SPlit vorher nachher = 0.8
 # set the context column as type 'factor' to get later all levels
-dataset$context <- as.factor(dataset$context)
+train$context <- as.factor(train$context)
 
 # List types for each attribute
-sapply(dataset,class)
+sapply(train,class)
 
 # View first five rows of the data
-head(dataset)
+head(train)
 
 # Get the levels to check what kind of classification problems we have (multi/binary)
-levels(dataset$context)
+levels(train$context)
 
 # Summarize the class distribution
-percentage<-prop.table(table(dataset$context)) * 100
-cbind(freq = table(dataset$context), percentage = percentage)
+percentage<-prop.table(table(train$context)) * 100
+cbind(freq = table(train$context), percentage = percentage)
 
 # Summarize attribute distributions
-summary(dataset)
+summary(train)
 
-###########################  STEP 4 - VIZUALIZATION ###########################
+###########################  STEP 5 - VIZUALIZATION ###########################
 
 # Split input and output x = alpha, beta, gamma, x, y, z
-x<-dataset[,2:7]
+x<-train[,1:6]
 
 # y = context
-y<-dataset[ ,9]
+y<-train[ ,7]
 
+plot(y)
 # Boxplot for each attribute on one image
-par(mfrow=c(2,7))
-for(i in 2:7) {
-  boxplot(x[,i], main=names(dataset)[i])
+par(mfrow=c(1,6))
+for(i in 1:6) {
+  boxplot(x[,i], main=names(train)[i])
 }
 
 # Barplot of class breakdown
 library(dplyr)
 library(ggplot2)
 
-dataset %>% ggplot(aes(x= y)) + geom_bar() +labs(x = "Context")
+train %>% ggplot(aes(x= y)) + geom_bar() +labs(y = "Context")
 
 # scatterplot matrix
 featurePlot(x=x, y=y, plot="ellipse")
@@ -116,32 +150,32 @@ featurePlot(x=x, y=y, plot="pairs")
 scales<-list(x = list(relation = "free"), y = list(relation = "free"))
 featurePlot(x = x, y = y, plot = "density", scales = scales)
 
-###########################  STEP 5 - ALGORITHM EVALUATION ###########################
+###########################  STEP 6 - ALGORITHM EVALUATION ###########################
 
 
 # Run algorithms using 10-fold cross validation
-control<-trainControl(method = "cv", number = 10)
+control<-trainControl(method = "cv", number = 10, repeats = 1)
 metric<-"Accuracy"
 
 # Linear Discriminant Analysis (LDA)  
 set.seed(7)
-fit.lda <- train(context~., data=dataset, method="lda", metric=metric, trControl=control)
+fit.lda <- train(context~., data=train, method="lda", metric=metric, trControl=control)
 
 # Classification and Regression Trees (CART)
 set.seed(7)
-fit.cart <- train(context~., data=dataset, method="rpart", metric=metric, trControl=control)
+fit.cart <- train(context~., data=train, method="rpart", metric=metric, trControl=control)
 
 # k-Nearest Neighbors (kNN)
 set.seed(7)
-fit.knn <- train(context~., data=dataset, method="knn", metric=metric, trControl=control)
+fit.knn <- train(context~., data=train, method="knn", metric=metric, trControl=control)
 
 # Support Vector Machines (SVM)
 set.seed(7)
-fit.svm <- train(context~., data=dataset, method="svmRadial", metric=metric, trControl=control)
+fit.svm <- train(context~., data=train, method="svmRadial", metric=metric, trControl=control)
 
 # Random Forest (RF)
 set.seed(7)
-fit.rf <- train(context~., data=dataset, method="rf", metric=metric, trControl=control)
+fit.rf <- train(context~., data=train, method="rf", metric=metric, trControl=control)
 
 # Summarize model accuracy for each model
 results <- resamples(list(lda=fit.lda,cart=fit.cart, knn=fit.knn, svm=fit.svm, rf=fit.rf))
@@ -151,22 +185,11 @@ summary(results)
 dotplot(results)
 
 # Summarize Best Model
-print(fit.knn)
+print(fit.rf)
 
 
-###########################  STEP 6 - PREDICTIONS ###########################
+###########################  STEP 7 - PREDICTIONS ###########################
 
 # Estimate skill of KNN on the validation dataset
-predictions<-predict(fit.knn, validation)
-confusionMatrix(predictions, validation$context)
-
-
-library(pmml)
-library(rpart)
-
-dataset$user <- as.factor(dataset$user)
-my.rpart <- rpart(context ~ ., data=dataset[,2:8])
-my.rpart
-
-pmml(my.rpart)
-saveXML(pmml(my.rpart), file = "my_rpart.xml")
+predictions<-predict(fit.rf, test)
+confusionMatrix(predictions, test$context)
